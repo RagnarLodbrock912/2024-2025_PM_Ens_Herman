66cfb7851592bb55a0593ec72ee95230caa4e1a3
RagnarLodbrock912 <vikingragnar912@gmail.com>
2024-11-14 15:54:08 +0200

    Add: lab_10

---
+++ lab_10/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+struct list {
+    int data;
+    list* next;
+
+    list(int value): data(value), next(nullptr) {}
+};
+
+
+int main() {
+    
+    return 0;
+}
