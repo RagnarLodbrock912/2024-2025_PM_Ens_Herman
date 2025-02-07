63a3b44216345204e5c46570285fce1c5c9b7ead
RagnarLodbrock912 <vikingragnar912@gmail.com>
2025-02-08 00:52:32 +0200

    Add: lab_07 C++

---
+++ sem_2/C++/lab_07/main.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <vector.hpp>
+#include <cstdint>
+
+using namespace std;
+
+class VectorBool {
+private:
+    Vector<uint64_t> bits;
+    size_t len;
+
+    void set_bit(size_t bit_index) {
+        bits[bit_index / 64] |= (1ULL << (bit_index % 64));
+    }
+
+    void clear_bit(size_t bit_index) {
+        bits[bit_index / 64] &= ~(1ULL << (bit_index % 64));
+    }
+
+    bool get_bit(size_t bit_index) const {
+        return (bits[bit_index / 64] >> (bit_index % 64)) & 1;
+    }
+public:
+    VectorBool() : len(0) {}
+
+    bool operator[](size_t index) const {
+        if (index >= len) throw out_of_range("Index out of range");
+        return get_bit(index);
+    }
+
+    void set(size_t index, bool value) {
+        if (index >= len) throw out_of_range("Index out of range");
+        if (value) {
+            set_bit(index);
+        } else {
+            clear_bit(index);
+        }
+    }
+
+    void push_back(bool value) {
+        if (len % 64 == 0) {
+            bits.push_back(0);
+        }
+        if (value) {
+            set_bit(len);
+        }
+        len++;
+    }
+
+    void insert(size_t index, bool value) {
+        if (index >= len) throw out_of_range("Index out of range");
+        push_back(false);
+        for (int i = len - 1; i > index; i--) {
+            bool tmp = (*this)[i - 1];
+            set(i, tmp);
+        }
+        set(index, value);  
+
+    }
+
+    void erase(size_t index) {
+        if (index >= len) throw out_of_range("Index out of range");
+        for (int i = index; i < len - 1; i++) {
+            bool tmp = (*this)[i + 1];
+            set(i, tmp);
+        }
+        len--;
+
+    }
+
+    size_t size() const {
+        return len;
+    }
+};
+
+int main() {
+    VectorBool v;
+
+    v.push_back(true);
+    v.push_back(false);
+    v.push_back(true);
+    v.push_back(false);
+    v.push_back(true);
+
+    std::cout << "Size: " << v.size() << std::endl;
+
+    for (size_t i = 0; i < v.size(); ++i) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+
+    v.set(2, false);
+
+    std::cout << "After changing index 2 to false: ";
+    for (size_t i = 0; i < v.size(); ++i) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+
+    v.insert(2, true);
+
+    std::cout << "After inserting true at index 2: ";
+    for (size_t i = 0; i < v.size(); ++i) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+
+    v.erase(1);
+
+    std::cout << "After erasing index 1: ";
+    for (size_t i = 0; i < v.size(); ++i) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ sem_2/C++/lab_07/vector.hpp
@@ -0,0 +1,208 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <cmath>
+
+using namespace std;
+
+int nextPowerOfTwo(int n) {
+    if (n <= 0) return 1; 
+    return pow(2, ceil(log2(n)));
+}
+
+template <typename vec>
+class Vector
+{
+private:
+    vec* arr;
+    size_t len;
+    size_t cap;
+public:
+    Vector() : arr(nullptr), len(0), cap(0) {}
+
+    Vector(int n, vec val = vec()) {
+        len = n;
+        cap = nextPowerOfTwo(n);
+        arr = new vec[cap];
+
+        for (size_t i = 0; i < len; i++) {
+            arr[i] = val;
+        }
+    }
+
+    Vector(const Vector& other) {
+        len = other.len;
+        cap = other.cap;
+        arr = new vec[cap];
+        for (size_t i = 0; i < len; i++) {
+            arr[i] = other.arr[i];
+        }
+    }
+
+    size_t size() const {
+        return len;
+    }
+
+    size_t capacity() const {
+        return cap;
+    }
+
+    ~Vector() {
+        delete[] arr;
+    }
+
+    Vector& resize(size_t n) {
+        if (n <= cap) {
+            len = n;
+            return *this;
+        }
+
+        vec* newArr = new vec[nextPowerOfTwo(n)];
+        size_t minLen = min(len, n);
+
+        for (size_t i = 0; i < minLen; i++) {
+            newArr[i] = arr[i];
+        }
+
+        delete[] arr;
+        arr = newArr;
+        len = n;
+        cap = nextPowerOfTwo(n);
+        return *this;
+    }
+
+    Vector& reserve(size_t n) {
+        if (n <= cap) return *this;
+
+        vec* newArr = new vec[nextPowerOfTwo(n)];
+        size_t minLen = min(len, n);
+
+        for (size_t i = 0; i < minLen; i++) {
+            newArr[i] = arr[i];
+        }
+
+        delete[] arr;
+        arr = newArr;
+        len = minLen;
+        cap = nextPowerOfTwo(n);
+        return *this;
+    }
+
+    Vector& shrink_to_fit() {
+        if (len == cap) return *this;
+
+        vec* newArr = new vec[len];
+        for (size_t i = 0; i < len; i++) {
+            newArr[i] = arr[i];
+        }
+
+        delete[] arr;
+        arr = newArr;
+        cap = len;
+        return *this;
+    }
+
+    Vector& reverse() {
+        for (size_t i = 0; i < len / 2; i++) {
+            swap(arr[i], arr[len - 1 - i]);
+        }
+        return *this;
+    }
+
+    vec* front() {
+        return len > 0 ? &arr[0] : nullptr;
+    }
+
+    vec* back() {
+        return len > 0 ? &arr[len - 1] : nullptr;
+    }
+
+    vec* begin() {
+        return len > 0 ? &arr[0] : nullptr;
+    }
+
+    vec* end() {
+        return len > 0 ? &arr[len] : nullptr;
+    }
+
+    bool empty() const {
+        return len == 0;
+    }
+
+    Vector& push_back(const vec& el) {
+        if (len + 1 > cap) this->reserve(nextPowerOfTwo(len + 1));
+        arr[len++] = el;
+        return *this;
+    }
+
+    Vector& insert(size_t index, const vec& el) {
+        if (index > len) {
+            throw out_of_range("Index out of range");
+        }
+        if (len + 1 > cap) this->reserve(nextPowerOfTwo(len + 1));
+        
+        for (size_t i = len; i > index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = el;
+        len++;
+        return *this;
+    }
+
+    Vector& erase(size_t index) {
+        if (index >= len) {
+            throw out_of_range("Index out of range");
+        }
+        for (size_t i = index; i < len - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        len--;
+        return *this;
+    }
+
+    vec& operator[](size_t i) {
+        if (i >= len) {
+            throw out_of_range("Index out of range");
+        }
+        return arr[i];
+    }
+
+    const vec& operator[](size_t i) const {
+        if (i >= len) {
+            throw out_of_range("Index out of range");
+        }
+        return arr[i];
+    }
+
+    Vector& operator=(const Vector& other) {
+        if (this == &other) return *this;
+
+        delete[] arr;
+
+        len = other.len;
+        cap = other.cap;
+        arr = new vec[cap];
+
+        for (size_t i = 0; i < len; i++) {
+            arr[i] = other.arr[i];
+        }
+
+        return *this;
+    }
+
+    friend ostream& operator<<(ostream& out, const Vector& array) {
+        for (int i = 0; i < array.len; i++) {
+            out << array.arr[i] << " ";
+        }
+        return out;
+    }
+
+    friend istream& operator>>(istream& in, const Vector& array) {
+        for (int i = 0; i < array.len; i++) {
+            in >> array.arr[i];
+        }
+        return in;
+    }
+};
+
+#endif
